fdc59eecb855c29ce772b906e59054f9daa25e79
Anna <ogonek0918@yandex.ru>
2022-12-06 18:34:26 +0700

    ask19

---
+++ task19/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+
+if ((num > 9999) && (num < 100000))
+{
+    int a = num / 100;
+    int e = a % 10;
+    int n = num / 10000;
+    int m = num % 10000;
+    int b = num / 1000 - n * 10;
+    int r = num / 10 - n*1000 - b*100 - e*10;
+    if ((n==m) || (b==r))
+    {
+        Console.WriteLine("Это палиндром");
+    }
+    else
+{
+    Console.WriteLine("Нет");
+}
+}
